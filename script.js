d012f57917d814978201daa95e6d0b92d4493803
Druvasena Reddy <66788819+druva-06@users.noreply.github.com>
2023-07-18 01:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let ans = '';
+  while (num != 0) {
+  	ans+=num%2
+	  num/=2
+  }
+	console.log(ans.reverse())
 }
 
 window.decimalToBinary = decimalToBinary;
